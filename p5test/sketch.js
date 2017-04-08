839a8c6c5e5136d509cda050abe3cb1f27faec6c
MagicGonads <andrelouisissa@gmail.com>
2017-04-08 21:12:52 +1000

    Multiple Canvasses

--- p5test/sketch.js
+++ p5test/sketch.js
@@ -1,60 +1,140 @@
-var plyMov;
-var plyPos;
-var plyAng = 0;
-const plySpd = 1;
-const plyRot = 0.125;
-const plyEnable = 1; 
-const plyRad = 25
-const canvasRatio = 0.8;
-var offX;
-var offY;
+var g = {};
+g.s1 = {}
+g.s2 = {}
+g.s3 = {}
+g.s4 = {} 
+g.plyAng=0
 
 function setup() {
-  createCanvas(int(windowWidth*canvasRatio), int(windowHeight*canvasRatio));
-  fill(0);
-	strokeWeight(2);
-	plyPos = createVector(0,0);
-	plyMov = createVector(0,0);
-	offX = width/2;
-	offY = height/2;
+	g.w=int(windowWidth*0.95)
+	g.h=int(windowHeight*0.95)
+	g.viewDistance=int(g.w*0.25)
+	g.widgetHeight=int(g.h*0.25)
+	g.plyRot=0.125
+	g.plySpd=1
+	g.plyRad=25
+  g.plyPos = createVector(0,0);
+  g.plyMov = createVector(0,0);
+	g.colBan = color(150)
+	g.colScr = color(0)
+	g.plrCol = color(255)
+	prep()
 }
 
-function draw() {
-	push();
-	translate (offX, offY);
-  background(0);
-  drawPly();
-	drawObj();
-	pop();
-}
-
-function drawPly(){
-	if(keyIsDown(32)){
-		plyMov.add(p5.Vector.fromAngle(plyAng).mult(plySpd));
-	}
-  plyPos.add(plyMov);
-	plyPos.set([constrain(plyPos.x,plyRad-offX,offX-plyRad),constrain(plyPos.y,plyRad-offY,offY-plyRad)]);
-  plyMov.set([0,0]);
-  stroke(255,0,0);
-	push()
-	translate(plyPos.x,plyPos.y)
-  ellipse(0,0,plyRad*2, plyRad*2);
-	const k = p5.Vector.fromAngle(plyAng).mult(plyRad);
-	line(0,0,k.x,k.y);
-	pop()
-}
-
-function drawObj(){
-  stroke(255);
-  ellipse(50, 50, 80, 80);
-}
-
-function mouseDragged(){
-	if (plyEnable){
-		plyAng+=(mouseX-pmouseX)*plyRot*TWO_PI/width;
-	}
+function prep(){
+	g.s1.w=g.w-2*g.viewDistance
+	g.s1.h=g.h-g.widgetHeight
+	g.s2.w=2*g.viewDistance
+	g.s2.h=g.s1.h
+	g.s3.w=g.s1.w
+	g.s4.h=g.h-g.widgetHeight
+	g.s4.w=g.s2.w
+	g.s4.h=g.s4.h
 }
 
 function windowResized() {
-  resizeCanvas(int(windowWidth*canvasRatio), int(windowHeight*canvasRatio));
+	location.reload(1)
 }
+
+function mouseDragged() {
+	g.plyAng+=(mouseX-pmouseX)*g.plyRot*TWO_PI/g.s1.w;
+}
+
+function keyPressed() {
+	
+}
+
+//Top Left
+new p5(function (p) {
+  "use strict";
+  var offX;
+  var offY;
+ 
+  p.setup = function () {
+    p.createCanvas(g.s1.w, g.s1.h);
+    p.fill(0);
+	  p.strokeWeight(2);
+	  offX = p.width/2;
+	  offY = p.height/2;
+  };
+ 
+  p.draw = function () {
+		p.background(g.colBan);
+  };
+},
+"sketch01");
+
+//Top Right
+new p5(function (p) {
+  "use strict";
+  var offX;
+  var offY;
+ 
+  p.setup = function () {
+    p.createCanvas(g.s2.w, g.s2.h);
+    p.fill(0);
+	  p.strokeWeight(2);
+	  offX = p.width/2;
+	  offY = p.height/2;
+  };
+ 
+  p.draw = function () {
+		p.background(g.colBan);
+  };
+},
+"sketch02");
+
+//Bottom Left
+new p5(function (p) {
+  "use strict";
+  var offX;
+  var offY;
+ 
+  p.setup = function () {
+    p.createCanvas(g.s2.w, g.s2.h);
+    p.fill(0);
+	  p.strokeWeight(2);
+	  offX = p.width/2;
+	  offY = p.height/2;
+  };
+ 
+  p.draw = function () {
+		p.background(g.colScr);
+  };
+},
+"sketch03");
+
+//Bottom Right
+new p5(function (p) {
+  "use strict";
+  var offX;
+  var offY;
+ 
+  p.setup = function () {
+    p.createCanvas(g.s4.w, g.s4.h);
+    p.fill(0);
+	  p.strokeWeight(2);
+	  offX = p.width/2;
+	  offY = p.height/2;
+  };
+ 
+  p.draw = function () {
+    p.push();
+	  p.translate (offX, offY);
+    p.background(g.colScr);
+		if(keyIsDown(32)){
+		  g.plyMov.add(createVector(1,0).rotate(g.plyAng).mult(g.plySpd));
+	  }
+    g.plyPos.add(g.plyMov);
+	  g.plyPos.set([constrain(g.plyPos.x,g.plyRad-offX,offX-g.plyRad),constrain(g.plyPos.y,g.plyRad-offY,offY-g.plyRad)]);
+    g.plyMov.set([0,0]);
+    p.stroke(g.plrCol);
+	  p.push()
+	  p.translate(g.plyPos.x,g.plyPos.y)
+    p.ellipse(0,0,g.plyRad*2, g.plyRad*2);
+	  const k = p.createVector(1,0).rotate(g.plyAng).mult(g.plyRad);
+	  p.line(0,0,k.x,k.y);
+	  p.pop()
+  };
+},
+"sketch04");
