613372fff4778136b07ad3cf2693db213d817170
MagicGonads <andrelouisissa@gmail.com>
2017-04-29 20:45:45 +1000

    changed layers and default nodes

--- compiterator/sketch.js
+++ compiterator/sketch.js
@@ -8,14 +8,20 @@ function setup() {
 	g.cr = 1
 	g.reps = 250
 	g.rad = 10
-	g.scal = 180
+	g.scal = 360
 	g.prob = 0.90
 	g.pnt = createVector(0,0)
 	g.attrs = [[
-		createVector(0,2),
-		createVector(sqrt(3),0),
-		createVector(-sqrt(3),0),
-	]]
+			createVector(0,2/sqrt(3)),
+			createVector(1,-1/sqrt(3)),
+			createVector(-1,-1/sqrt(3)),
+		],
+		[
+			createVector(0,-2/sqrt(3)),
+			createVector(1,1/sqrt(3)),
+			createVector(-1,1/sqrt(3)),
+		]
+	]
 	createCanvas(windowWidth*g.cr,windowHeight*g.cr)
 	g.g = createGraphics(width,height)
 	t = min(width,height)/(925*g.cr)
@@ -30,6 +36,7 @@ function setup() {
 	angleMode(DEGREES)
 	textSize(20*t)
 	textFont('Consolas')
+	g.set=g.attrs[0]
 	g.addr=0
 	g.saddr=0
 	g.edit=1
@@ -89,9 +96,10 @@ function draw() {
 		}
 		for (var i = 0;i<g.reps;i++){
 			if (random(1)>g.prob){
-				g.saddr=(g.saddr+1)%g.attrs.length
+				//g.saddr=(g.saddr+1)%g.attrs.length
+				g.set = random(g.attrs)
 			}
-			g.set = g.attrs[g.saddr]
+			//g.set = g.attrs[g.saddr]
 			g.attr = random(g.set)
 			h = g.pnt.heading()
 			if (h<0){
