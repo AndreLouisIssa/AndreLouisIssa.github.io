156e5b64399f3c9525c5e31897f53784d76d9e0d
MagicGonads <andrelouisissa@gmail.com>
2017-04-29 20:01:15 +1000

    Control display, memory rendering

--- compiterator/sketch.js
+++ compiterator/sketch.js
@@ -5,10 +5,11 @@ function iterate() {
 }
 
 function setup() {
-	g.cr = 0.95
+	g.cr = 1
 	g.reps = 250
-	g.rad = 1
+	g.rad = 10
 	g.scal = 180
+	g.prob = 0.90
 	g.pnt = createVector(0,0)
 	g.attrs = [[
 		createVector(0,2),
@@ -16,19 +17,28 @@ function setup() {
 		createVector(-sqrt(3),0),
 	]]
 	createCanvas(windowWidth*g.cr,windowHeight*g.cr)
+	g.g = createGraphics(width,height)
+	t = min(width,height)/(925*g.cr)
+	g.scal = g.scal*t
+	g.rad = g.rad*t
 	g.x = width/2
 	g.y = height/2
 	background(0)
-	noStroke()
+	g.g.noStroke()
 	colorMode(HSB,360)
+	g.g.colorMode(HSB,360)
 	angleMode(DEGREES)
+	textSize(20*t)
+	textFont('Consolas')
 	g.addr=0
+	g.saddr=0
+	g.edit=1
 }
 
 function draw() {
+	background(0)
 	translate(g.x,g.y)
 	if (g.edit) {
-		background(0)
 		len = g.attrs.length
 		brk = []
 		brek = 1
@@ -40,7 +50,7 @@ function draw() {
 				brek = 1
 				if(mouseIsPressed){
 					tempos = createVector(mouseX-g.x,-mouseY+g.y).div(g.scal)
-					if (g.scal*tempos.dist(attr)<g.rad*5) {
+					if (g.scal*tempos.dist(attr)<g.rad) {
 						if (attrs.length>1){ 
 								brk.push([i,k])
 								brek = 0
@@ -49,26 +59,49 @@ function draw() {
 				}
 				if(brek){
 					fill(360*i/len,360,360)
-					ellipse(attr.x*g.scal,-attr.y*g.scal,g.rad*5,g.rad*5)
+					ellipse(attr.x*g.scal,-attr.y*g.scal,g.rad,g.rad)
 				}
 			}
 		}
 		for (var i = 0; i<brk.length;i++){
 		g.attrs[brk[i][0]].splice(brk[i][1],1)
 		}
+		if(!(mouseX>-g.x/4+g.x+g.rad && mouseX<-g.x/4+g.x+g.x/2-g.rad && mouseY>g.y/2+g.y+g.rad)){
+			fill(10)
+			rect(-g.x/4-g.rad,g.y/2-g.rad,g.x/2+g.rad,g.y/2+g.rad)
+			fill(360)
+			text(
+				'Controls \n'+
+				'Space: Toggle Render/Edit \n'+
+				'ALT (Hold) + LMB: Add \n'+
+				'LMB (Hold): Delete \n'+
+				'Up: New Layer \n'+
+				'Down: Delete Top Layer \n'+
+				'Right: Next Layer \n'+
+				'Left: Previous Layer \n'
+			,-g.x/4,g.y/2,g.x/2,g.y/2)
+		}	
 	}
 	else {
+		if(mouseIsPressed){
+			g.pnt.x+=(mouseX-g.x)/g.scal*2
+			g.pnt.y-=(mouseY-g.y)/g.scal*2
+		}
 		for (var i = 0;i<g.reps;i++){
-			g.set = random(g.attrs)
+			if (random(1)>g.prob){
+				g.saddr=(g.saddr+1)%g.attrs.length
+			}
+			g.set = g.attrs[g.saddr]
 			g.attr = random(g.set)
 			h = g.pnt.heading()
 			if (h<0){
 				h+=360
 			}
-			fill(h,360,360)
+			g.g.fill(h,360,360)
 			iterate()
-			ellipse(g.pnt.x*g.scal,-g.pnt.y*g.scal,g.rad,g.rad)
+			g.g.ellipse(g.pnt.x*g.scal+g.x,-g.pnt.y*g.scal+g.y,0.25,0.25)
 		}
+		image(g.g,-g.x,-g.y,width,height)
 	}
 }
 
@@ -76,12 +109,11 @@ function windowResized() {
 	resizeCanvas(windowWidth*g.cr,windowHeight*g.cr)
 	g.x = width/2
 	g.y = height/2
-	background(0)
+	g.g.clear()
 }
 
 function keyPressed() {
-	console.log(keyCode)
-	if (keyCode==13){
+	if (keyCode==13 && !g.edit){
 		saveCanvas('compiterator.png')
 	}
 	if (keyCode==18){
@@ -89,7 +121,8 @@ function keyPressed() {
 	}
 	if (keyCode==32){
 		g.edit = !g.edit
-		background(0)
+		g.saddr=0
+		windowResized()
 	}
 	if (g.edit){
 		switch (keyCode){
