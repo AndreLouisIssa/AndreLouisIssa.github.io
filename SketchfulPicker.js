65ff2440ca030290a1a94143276229e8c0815939
Andre Issa <andrelouisissa@gmail.com>
2020-06-17 03:14:04 +1000

    Update SketchfulPicker.js

--- SketchfulPicker.js
+++ SketchfulPicker.js
@@ -19,16 +19,25 @@ How to use:
 If having trouble getting it to load correctly, try changing the `0` at the bottom of the script to something else like `300`
 */
 
+    //To add a custom palette page, copy the array of hex codes and replace with your own codes
+
+let palettes = [
+    [
+        "#3a3a3c", "#8e8e93", "#f8f9fa", "#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff", "#fdad88", "#9e5425",
+        "#2c2c2e", "#636366", "#e0e0e0", "#ff7070", "#f3a220", "#f9e079", "#049d6f", "#92ddea", "#6dafe0", "#ab87ff", "#ff87ab", "#e38a5e", "#5e320d",
+        "#1c1c1e", "#48484a", "#c2c2c2", "#f50000", "#dc8700", "#f0c808", "#00766a", "#219bc3", "#548bbc", "#715aff", "#ff5d8f", "#d1754e", "#421e06"
+    ],
+/*
+    [
+        "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000",
+        "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000",
+        "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"
+    ],
+*/
+];
+
 setTimeout(function() {
     'use strict';
-
-    let palettes = [
-        [
-            "#3a3a3c", "#8e8e93", "#f8f9fa", "#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff", "#fdad88", "#9e5425",
-            "#2c2c2e", "#636366", "#e0e0e0", "#ff7070", "#f3a220", "#f9e079", "#049d6f", "#92ddea", "#6dafe0", "#ab87ff", "#ff87ab", "#e38a5e", "#5e320d",
-            "#1c1c1e", "#48484a", "#c2c2c2", "#f50000", "#dc8700", "#f0c808", "#00766a", "#219bc3", "#548bbc", "#715aff", "#ff5d8f", "#d1754e", "#421e06"
-        ]
-    ];
 
     function rgb2hex(rgb) {
         if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
