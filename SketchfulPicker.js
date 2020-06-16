f087518186b4bfd18102a4ecd8396cbf0ff54120
Andre Issa <andrelouisissa@gmail.com>
2020-06-17 00:03:15 +1000

    Update SketchfulPicker.js

--- SketchfulPicker.js
+++ SketchfulPicker.js
@@ -22,9 +22,6 @@ If having trouble getting it to load correctly, try changing the `0` at the bott
 setTimeout(function() {
     'use strict';
 
-    const COLMAX = 39;
-    const PCOLDEFAULT = "radial-gradient(#000,#FFF)";
-
     let palettes = [
         [
             "#3a3a3c", "#8e8e93", "#f8f9fa", "#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff", "#fdad88", "#9e5425",
@@ -33,16 +30,9 @@ setTimeout(function() {
         ]
     ];
 
-    palettes.unshift([]);
-    palettes.push([]);
-    var j;
-    for (j = 0; j < COLMAX; j++) {
-        palettes[0].push(document.getElementsByClassName("gameToolsColor")[j].style.background);
-        palettes[palettes.length-1].push(PCOLDEFAULT);
-    }
-
     function rgb2hex(rgb) {
         if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+        if (!/^rgb/i.test(rgb)) return "currentcolor";
 
         rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
         function hex(x) {
@@ -52,95 +42,133 @@ setTimeout(function() {
     }
 
     function invert(rgb) {
+        if (!/^rgb/i.test(rgb)) return "currentcolor";
+
         rgb = Array.prototype.join.call(arguments).match(/(-?[0-9\.]+)/g);
-        for (var i = 0; i < rgb.length; i++) {
+        for (let i = 0; i < rgb.length; i++) {
             rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
         }
         return "rgb("+rgb+")";
     }
 
-    document.getElementById("gameToolsColorPreview").setAttribute ('style',"position: relative; top:-25px");
+    palettes.unshift([]);
+    let nodes = document.getElementsByClassName("gameToolsColor");
+    for (let j = 0; j < nodes.length; j++) {
+        let x = nodes[j];
+        palettes[0].push(x.style.background);
+        x.addEventListener("click", function() { clickedSwatch(j); }, false);
+    }
 
-    var zNode = document.createElement ('input');
-    zNode.setAttribute ('id', 'picker');
-    zNode.setAttribute ('class', "jscolor {mode:\"HVS\"}");
-    zNode.setAttribute ('value',"ffffff")
-    zNode.setAttribute ('style',"position: relative; text-align: center; width:30%; top:-29.5%; height:30%; border:1px solid black");
-    document.getElementById("gameToolsColors").appendChild(zNode);
+    document.getElementById("gameToolsColorPreview").setAttribute('style',"position: relative; top:-32px");
+    setInterval( function() { document.getElementById("gameChat").setAttribute('style',"height: calc(100% - 210px);"); }, 1500);
 
-    zNode = document.createElement ('span');
-    zNode.setAttribute ('id', 'cstore');
-    zNode.setAttribute ('role', 'button');
-    zNode.setAttribute ('class', "btn btn-success btn-sm");
-    zNode.innerHTML = "<div style = \"font-size: 125%; position: relative; left:0%; top:-30%\">+</div>";
-    zNode.setAttribute ('style',"position: relative; width:15%; left:5%; top:-58.5%; height:10%");
-    document.getElementById("gameToolsColors").appendChild(zNode);
+    let anchor = document.getElementById("gameToolsColors");
 
-    zNode = document.createElement ('span');
-    zNode.setAttribute ('id', 'cppage');
-    zNode.setAttribute ('role', 'button');
-    zNode.setAttribute ('class', "btn btn-danger btn-sm");
-    zNode.innerHTML = "<div style = \"font-size: 125%; position: relative; left:-30%; top:-30%\">\<</div>";
-    zNode.setAttribute ('style',"position: relative; width:10%; left:10%; top:-58.5%; height:10%");
-    document.getElementById("gameToolsColors").appendChild(zNode);
+    let nodePicker = document.createElement ('input');
+    nodePicker.setAttribute ('id', 'picker');
+    nodePicker.setAttribute ('class', `jscolor {onFineChange:"document.getElementById('picker').oninput()"}`);
+    nodePicker.setAttribute ('value',"ffffff");
+    nodePicker.oninput = function() {
+        swapColor(0);
+    }
+    nodePicker.setAttribute ('style',"position: relative; text-align: center; left: 1%; width:30%; top:-30%; height:30%; border:0px solid currentcolor");
+    anchor.appendChild(nodePicker);
 
-    zNode = document.createElement ('span');
-    zNode.setAttribute ('id', 'cnpage');
-    zNode.setAttribute ('role', 'button');
-    zNode.setAttribute ('class', "btn btn-primary btn-sm");
-    zNode.innerHTML = "<div style = \"font-size: 125%; position: relative; left:-30%; top:-30%\"\>></div>";
-    zNode.setAttribute ('style',"position: relative; width:10%; left:10%; top:-58.5%; height:105%");
-    document.getElementById("gameToolsColors").appendChild(zNode);
+    let nodeSave = document.createElement ('span');
+    nodeSave.setAttribute ('id', 'cstore');
+    nodeSave.setAttribute ('role', 'button');
+    nodeSave.setAttribute ('class', "btn btn-success btn-sm");
+    nodeSave.innerHTML = "<div style = \"font-size: 125%; position: relative; left:0%; top:-30%\">=</div>";
+    nodeSave.setAttribute ('style',"position: relative; width:15%; left:5%; top:-58.5%; height:1%");
+    anchor.appendChild(nodeSave);
 
-    var gp = document.getElementById('picker')
-    document.getElementById('cstore').addEventListener("click", makeColour, false);
-    document.getElementById('cppage').addEventListener("click", prevPage, false);
-    document.getElementById('cnpage').addEventListener("click", nextPage, false);
+    let nodeNew = document.createElement ('span');
+    nodeNew.setAttribute ('id', 'capage');
+    nodeNew.setAttribute ('role', 'button');
+    nodeNew.setAttribute ('class', "btn btn-success btn-sm");
+    nodeNew.innerHTML = "<div style = \"font-size: 125%; position: relative; left:0%; top:-30%\">+</div>";
+    nodeNew.setAttribute ('style',"position: relative; width:15%; left:5%; top:-58.5%; height:1%");
+    anchor.appendChild(nodeNew);
 
-    var i = 0;
-    j = 0;
+    let nodePrev = document.createElement ('span');
+    nodePrev.setAttribute ('id', 'cppage');
+    nodePrev.setAttribute ('role', 'button');
+    nodePrev.setAttribute ('class', "btn btn-danger btn-sm");
+    nodePrev.innerHTML = "<div style = \"font-size: 125%; position: relative; left:0%; top:-30%\">\<</div>";
+    nodePrev.setAttribute ('style',"position: relative; width:15%; left:10%; top:-58.5%; height:1%");
+    anchor.appendChild(nodePrev);
 
-    var pj = 0;
-    function makeColour(zEvent){
-        if (gp.style.backgroundColor) {
-            var col = rgb2hex(gp.style.backgroundColor)
-            palettes[palettes.length-1][pj] = col;
-            if (i == palettes.length-1) {
-                document.getElementsByClassName("gameToolsColor")[pj].setAttribute("style","background:"+col);
-            }
-            pj=(pj+1)%(palettes[palettes.length-1].length);
-            if (i == palettes.length-1) {
-                var pcol = document.getElementsByClassName("gameToolsColor")[pj].style.background;
-                document.getElementsByClassName("gameToolsColor")[pj].setAttribute("style","background:"+pcol+"; border-color:"+invert(pcol));
-            }
+    let nodeNext = document.createElement('span');
+    nodeNext.setAttribute ('id', 'cnpage');
+    nodeNext.setAttribute ('role', 'button');
+    nodeNext.setAttribute ('class', "btn btn-primary btn-sm");
+    nodeNext.innerHTML = "<div style = \"font-size: 125%; position: relative; left:0%; top:-30%\"\>></div>";
+    nodeNext.setAttribute ('style',"position: relative; width:15%; left:10%; top:-58.5%; height:1%");
+    anchor.appendChild(nodeNext);
+
+    nodePicker.addEventListener("click", swapColor, false);
+    nodeSave.addEventListener("click", makeColor, false);
+    nodeNew.addEventListener("click", makePage, false);
+    nodePrev.addEventListener("click", prevPage, false);
+    nodeNext.addEventListener("click", nextPage, false);
+
+    let i = 0;
+    let j = 0;
+    let pj = 0;
+
+    function clickedSwatch(k){
+        console.log("page "+i+" node "+k+":"+rgb2hex(nodes[k].style.background));
+        nodes[pj].setAttribute("style","background:"+palettes[i][pj]);
+        pj = k;
+        nodes[pj].setAttribute("style","background:"+palettes[i][pj]+"; border-color:"+invert(palettes[i][pj]));
+    }
+
+    function swapColor(zEvent){
+        let pcol = nodes[0].style.background;
+        let col = rgb2hex(nodePicker.style.backgroundColor)
+        nodes[0].style.background=col;
+        nodes[0].dispatchEvent(new Event("pointerdown"));
+        nodes[0].style.background=pcol;
+    }
+
+    function makeColor(zEvent){
+        let col = rgb2hex(nodePicker.style.backgroundColor)
+        palettes[i][pj] = col;
+        nodes[pj].style.background=col;
+    }
+
+    function makePage(zEvent){
+        palettes.push([]);
+        for (let j = 0; j < nodes.length; j++) {
+            palettes[palettes.length-1].push("radial-gradient(#000,#FFF)");
         }
     }
 
     function prevPage(zEvent){
         i = (i-1+palettes.length)%palettes.length;
-        for (j = 0; j < COLMAX; j++) {
-            document.getElementsByClassName("gameToolsColor")[j].style.background = palettes[i][j];
+        for (j = 0; j < nodes.length; j++) {
+            nodes[j].style.background = palettes[i][j];
         }
-        var pcol = document.getElementsByClassName("gameToolsColor")[pj].style.background;
+        let pcol = nodes[pj].style.background;
         if (i == palettes.length-1) {
-            document.getElementsByClassName("gameToolsColor")[pj].setAttribute("style","background:"+pcol+"; border-color:"+invert(pcol));
+            nodes[pj].setAttribute("style","background:"+pcol+"; border-color:"+invert(pcol));
         }
         else {
-            document.getElementsByClassName("gameToolsColor")[pj].setAttribute("style","background:"+pcol);
+            nodes[pj].setAttribute("style","background:"+pcol);
         }
     }
 
     function nextPage(zEvent){
         i = (i+1)%palettes.length;
-        for (j = 0; j < COLMAX; j++) {
-            document.getElementsByClassName("gameToolsColor")[j].style.background = palettes[i][j];
+        for (j = 0; j < nodes.length; j++) {
+            nodes[j].style.background = palettes[i][j];
         }
-        var pcol = document.getElementsByClassName("gameToolsColor")[pj].style.background;
+        let pcol = nodes[pj].style.background;
         if (i == palettes.length-1) {
-            document.getElementsByClassName("gameToolsColor")[pj].setAttribute("style","background:"+pcol+"; border-color:"+invert(pcol));
+            nodes[pj].setAttribute("style","background:"+pcol+"; border-color:"+invert(pcol));
         }
         else {
-            document.getElementsByClassName("gameToolsColor")[pj].setAttribute("style","background:"+pcol);
+            nodes[pj].setAttribute("style","background:"+pcol);
         }
     }
 
