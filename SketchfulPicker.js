9c182738e960b0fd97a143a611fa1cc46e5f5b3a
Andre Issa <andrelouisissa@gmail.com>
2020-08-01 20:20:19 +1000

    Updated for latest JScolor, includes chat resizer

--- SketchfulPicker.js
+++ SketchfulPicker.js
@@ -22,11 +22,26 @@ If having trouble getting it to load correctly, try changing the `0` at the bott
     //To add a custom palette page, copy the array of hex codes and replace with your own codes
 
 let palettes = [
-    [
-        "#3a3a3c", "#8e8e93", "#f8f9fa", "#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff", "#fdad88", "#9e5425",
-        "#2c2c2e", "#636366", "#e0e0e0", "#ff7070", "#f3a220", "#f9e079", "#049d6f", "#92ddea", "#6dafe0", "#ab87ff", "#ff87ab", "#e38a5e", "#5e320d",
-        "#1c1c1e", "#48484a", "#c2c2c2", "#f54d4d", "#dc8700", "#f0c808", "#00766a", "#219bc3", "#548bbc", "#715aff", "#ff5d8f", "#d1754e", "#421e06"
-    ],
+	[
+		'#3a3a3c', '#8e8e93', '#f8f9fa', '#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff', '#fdad88', '#9e5425',
+		'#2c2c2e', '#636366', '#e0e0e0', '#ff7070', '#f3a220', '#f9e079', '#049d6f', '#92ddea', '#6dafe0', '#ab87ff', '#ff87ab', '#e38a5e', '#5e320d',
+		'#1c1c1e', '#48484a', '#c2c2c2', '#f54d4d', '#dc8700', '#f0c808', '#00766a', '#219bc3', '#548bbc', '#715aff', '#ff5d8f', '#d1754e', '#421e06',
+	],
+	[
+		'#081c15', '#1b4332', '#2d6a4f', '#40916c', '#52b788', '#74c69d', '#95d5b2', '#b7e4c7', '#d8f3dc', '#000000', '#faf9f9', '#ffd6ba', '#fec89a',
+		'#774936', '#8a5a44', '#9d6b53', '#b07d62', '#c38e70', '#cd9777', '#d69f7e', '#deab90', '#e6b8a2', '#edc4b3', '#ffb5a7', '#fcd5ce', '#f8edeb',
+		'#cb997e', '#eddcd2', '#fff1e6', '#f0efeb', '#ddbea9', '#a5a58d', '#b7b7a4', '#6d6875', '#b5838d', '#e5989b', '#ffb4a2', '#ffcdb2', '#f9dcc4',
+	],
+	[
+		'#10002b', '#240046', '#3c096c', '#5a189a', '#7b2cbf', '#9d4edd', '#c77dff', '#e0aaff', '#efcefa', '#d4b2d8', '#a88fac', '#826c7f', '#5d4e60',
+		'#7c6f93', '#886f93', '#a967ad', '#ad6789', '#db81ad', '#ff6c91', '#ff736c', '#ff9e46', '#faa275', '#ff8c61', '#ce6a85', '#985277', '#5c374c',
+		'#721b65', '#b80d57', '#f8615a', '#ffd868', '#bb596b', '#f96d80', '#ff9a76', '#ffc4a3', '#00e0ff', '#74f9ff', '#a6fff2', '#e8ffe8', '#ffffff',
+	],
+	[
+		'#007f5f', '#2b9348', '#55a630', '#80b918', '#aacc00', '#bfd200', '#d4d700', '#dddf00', '#eeef20', '#ffff3f', '#03045e', '#0077b6', '#00b4d8',
+		'#ff4800', '#ff5400', '#ff6000', '#ff6d00', '#ff7900', '#ff8500', '#ff9100', '#ff9e00', '#ffaa00', '#ffb600', '#90e0ef', '#caf0f8', '#000000',
+		'#143642', '#263c41', '#38413f', '#4a473e', '#5c4d3c', '#6f523b', '#815839', '#935e38', '#a56336', '#b76935', '#000000', '#ffffff', '#ffffff',
+	],
 /*
     [
         "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000",
@@ -60,6 +75,30 @@ setTimeout(function() {
         return "rgb("+rgb+")";
     }
 
+    //Code courtesy of Bell
+    const gameTools = document.querySelector('#gameTools');
+    const chatBox = document.querySelector('#gameChat');
+
+    function addObservers() {
+        const heightObserver = new MutationObserver(adjustChatSize);
+        const config = {
+            attributes: true,
+        };
+        heightObserver.observe(gameTools, config);
+        heightObserver.observe(chatBox, config);
+    }
+
+    function adjustChatSize() {
+        chatBox.style.height = isDrawing() ? 'calc(100% - 210px)' : 'calc(100% - 180px)';
+    }
+
+    function isDrawing() {
+        return document.querySelector('#gameTools').style.display !== 'none';
+    }
+
+    addObservers()
+    //
+
     palettes.unshift([]);
     let nodes = document.getElementsByClassName("gameToolsColor");
     for (let j = 0; j < nodes.length; j++) {
@@ -68,19 +107,22 @@ setTimeout(function() {
         x.addEventListener("click", function() { clickedSwatch(j); }, false);
     }
 
-    document.getElementById("gameToolsColorPreview").setAttribute('style',"position: relative; top:-32px");
-    //setInterval( function() { document.getElementById("gameChat").setAttribute('style',"height: calc(100% - 210px);"); }, 1500);
-
     let anchor = document.getElementById("gameToolsColors");
+    document.getElementById("gameToolsColorPreview").setAttribute('style',"position: relative; top:-32px");
 
     let nodePicker = document.createElement ('input');
     nodePicker.setAttribute ('id', 'picker');
-    nodePicker.setAttribute ('class', `jscolor {onFineChange:"document.getElementById('picker').oninput()"}`);
+    nodePicker.setAttribute ('data-jscolor', '{hash:false, previewElement:null}');
     nodePicker.setAttribute ('value',"ffffff");
     nodePicker.oninput = function() {
-        swapColor(0);
+        // Code courtesy of EastDesire
+        this.style.backgroundColor = this.jscolor.toHEXString();
+        this.style.color = this.jscolor.isLight() ? 'black' : 'white';
+        //
+        scol = this.style.backgroundColor
+        swapColor();
     }
-    nodePicker.setAttribute ('style',"position: relative; text-align: center; left: 1%; width:30%; top:-30%; height:30%; border:0px solid currentcolor");
+    nodePicker.setAttribute ('style',"position: relative; text-align: center; left: 1%; width:30%; border:0px solid currentcolor");
     anchor.appendChild(nodePicker);
 
     let nodeSave = document.createElement ('span');
@@ -123,27 +165,34 @@ setTimeout(function() {
 
     let i = 0;
     let j = 0;
-    let pj = 0;
+    let pj = 26;
+    let scol = palettes[i][pj];
     nodes[pj].setAttribute("style","background:"+palettes[i][pj]+"; border-color:"+invert(palettes[i][pj]));
 
 
     function clickedSwatch(k){
-        console.log("page "+i+" node "+k+":"+rgb2hex(nodes[k].style.background));
+        let col = rgb2hex(nodes[k].style.background);
+        console.log("page "+i+" node "+k+":"+col);
         nodes[pj].setAttribute("style","background:"+palettes[i][pj]);
         pj = k;
         nodes[pj].setAttribute("style","background:"+palettes[i][pj]+"; border-color:"+invert(palettes[i][pj]));
+        if (col=="currentcolor"){
+            swapColor();
+        }
+        else {
+            scol = col;
+        }
     }
 
-    function swapColor(zEvent){
+    function swapColor(){
         let pcol = nodes[0].style.background;
-        let col = rgb2hex(nodePicker.style.backgroundColor)
-        nodes[0].style.background=col;
+        nodes[0].style.background=scol;
         nodes[0].dispatchEvent(new Event("pointerdown"));
         nodes[0].style.background=pcol;
     }
 
     function makeColor(zEvent){
-        let col = rgb2hex(nodePicker.style.backgroundColor)
+        let col = nodePicker.jscolor.toHEXString();
         palettes[i][pj] = col;
         nodes[pj].style.background=col;
     }
@@ -171,4 +220,5 @@ setTimeout(function() {
         nodes[pj].setAttribute("style","background:"+palettes[i][pj]+"; border-color:"+invert(palettes[i][pj]));
     }
 
+    jscolor.install();
 }, 0);
